6ccf925ce06a64a8dcabde9d18ff5d2afde8d8ba
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-06 18:21:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -17,4 +17,13 @@ countdown(12)
 
 def countdown_with_sleep(n)
   
+    while n > 0 
+    puts "#{n} SECOND(S)!"
+    n -= 1 
+  end
+  
+  return "HAPPY NEW YEAR!"
+  
 end
+
+countdown_with_sleep()
